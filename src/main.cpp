cb74409cd6fe46d76b0bee5dc74c7b52b5b985b2
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2026-05-12 00:51:37 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,20 @@
 #include <string>
 #include <unistd.h>
 #include <sstream>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<vector>
 using namespace std;
 
-
 string executable(string command){
-
-  char* pathEnv= getenv("PATH");
+  char* pathEnv=getenv("PATH");
   string path(pathEnv);
-  
-  stringstream ss(path);
+  stringstream ss;
   string dir;
   while(getline(ss,dir,':')){
-    string fullPath=dir+"/"+command;
-    if(access(fullPath.data(),F_OK)==0){
-      if(access(fullPath.data(),X_OK)==0){
+    string fullPath=dir+'/'+command;
+    if(access(fullPath.data(),F_OK)){
+      if(access(fullPath.data(),X_OK)){
         return fullPath;
       }
     }
@@ -23,29 +23,54 @@ string executable(string command){
   return "";
 }
 
+
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   while(true){
-    std::cout << "$ ";
-    string command;
-    getline(cin,command);
-    if(command=="exit") break;
-    else if(command.substr(0,5)=="echo ") cout<<command.substr(5)<<"\n";
+    cout<<"$";
+    string line;
+    getline(cin,line);
+    stringstream ss;
+    vector<string> tokens;
+    string word;
 
-    else if(command.substr(0,5)=="type "){
-      string commandType;
-      commandType=command.substr(5);
-      if(commandType=="echo" || commandType=="exit" || commandType=="type"){
-        cout<<commandType<<" is a shell builtin\n";
-      }
-      else if(executable(commandType)!=""){
-        cout<<commandType<<" is "<<executable(commandType)<<"\n";
-      }
-      else cout<<commandType<<": not found\n";
+    while(ss >> word){
+      tokens.push_back(word);
     }
-
-    else cout<<command<<": command not found\n";
+    if(tokens[0]=="exit") break;
+    else if(tokens[0]=="echo"){
+      for(int i=1;i<tokens.size();i++){
+        cout<<tokens[i]<<" ";
+      }
+      cout<<endl;
+    }
+    else if(tokens[0]=="type"){
+      if(tokens[1]=="echo" || tokens[1]=="type" || tokens[1]=="exit"){
+        cout<<tokens[1]<<"is a shell builtin\n";
+      }else if(executable(tokens[1])!=""){
+        cout<<tokens[1]<<" is "<<executable(tokens[1]);
+      }else{
+        cout<<tokens[1]<<": not found\n";
+      }
+    }
+    else if(executable(tokens[0])!=""){
+      pid_t pid=fork();
+      if(pid==0){
+        vector<char*> args;
+        for(auto& token:tokens){
+          args.push_back(token.data());
+        }
+        execvp(args[0],args.data());
+      }else{
+        waitpid(pid,NULL,0);
+      }
+      
+    }
+    else{
+      cout<<tokens[0]<<": command no found";
+    }
   }
+  
 }
